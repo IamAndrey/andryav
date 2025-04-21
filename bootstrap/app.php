4ac4eea42b8248920703a251ea295276e115656e
Andrey <abramenko.av86@gmail.com>
2025-04-21 21:44:57 +0200

    Added CookieConsentMiddleware library

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Middleware\AddHeaders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\CookieConsent\CookieConsentMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(AddHeaders::class);
+        $middleware->append(CookieConsentMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
